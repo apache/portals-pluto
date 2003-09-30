6d99f7496fe42eed1500c111d1c48563239dc669
David Sean Taylor <taylor@apache.org>
2003-09-30 17:30:17 +0000

    removed extra lines
    git-svn-id: https://svn.apache.org/repos/asf/portals/pluto/trunk@35769 13f79535-47bb-0310-9956-ffa450edef68

--- docs/create_object_model.cmd
+++ docs/create_object_model.cmd
@@ -1,4 +1,1 @@
-rmdir /S /Q docs
-mkdir docs
-javadoc -classpath ../lib/j2ee.jar;../src/container -d docs -windowtitle "Pluto Object Model" -header "Pluto Object Model" org.apache.pluto.om org.apache.pluto.om.common org.apache.pluto.om.entity org.apache.pluto.om.portlet org.apache.pluto.om.servlet
-pause
+rmdir /S /Q docsmkdir docsjavadoc -classpath ../lib/j2ee.jar;../src/container -d docs -windowtitle "Pluto Object Model" -header "Pluto Object Model" org.apache.pluto.om org.apache.pluto.om.common org.apache.pluto.om.entity org.apache.pluto.om.portlet org.apache.pluto.om.servletpause
