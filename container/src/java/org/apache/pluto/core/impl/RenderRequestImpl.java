518204944ddbddb48206d04623ca4363f84a3b6b
David H. DeWolf <ddewolf@apache.org>
2004-02-24 22:59:55 +0000

    Removing System.out.println.  Added Logging with code guards.
    git-svn-id: https://svn.apache.org/repos/asf/portals/pluto/trunk@35897 13f79535-47bb-0310-9956-ffa450edef68

--- container/src/java/org/apache/pluto/core/impl/RenderRequestImpl.java
+++ container/src/java/org/apache/pluto/core/impl/RenderRequestImpl.java
@@ -66,6 +66,9 @@ import javax.portlet.RenderRequest;
 
 import org.apache.pluto.factory.PortletObjectAccess;
 import org.apache.pluto.om.window.PortletWindow;
+import org.apache.pluto.services.log.LogService;
+import org.apache.pluto.services.log.Logger;
+import org.apache.pluto.PortletContainerServices;
 
 /**
 
@@ -80,11 +83,14 @@ public class RenderRequestImpl extends PortletRequestImpl implements RenderReque
      * Holds the portlet preferences
      */
     private PortletPreferences portletPreferences = null;
+    private Logger log = null;
 
     public RenderRequestImpl(PortletWindow portletWindow,
                              javax.servlet.http.HttpServletRequest servletRequest)
     {
         super(portletWindow, servletRequest);
+        this.log = ((LogService)PortletContainerServices
+                      .get(LogService.class)).getLogger(getClass());
     }
 
     // additional methods -------------------------------------------------------------------------
@@ -97,10 +103,27 @@ public class RenderRequestImpl extends PortletRequestImpl implements RenderReque
     }
 
     public PortletPreferences getPreferences()
-    {        System.out.println("**** Getting Prefs " + portletPreferences);
+    {
+        if(log.isDebugEnabled()) {
+            log.debug("Getting Preferences: "+portletPreferences);
+        }
+
         if (portletPreferences == null) {
-            portletPreferences = PortletObjectAccess.getPortletPreferences(org.apache.pluto.Constants.METHOD_RENDER, super.getInternalPortletWindow().getPortletEntity());
-        }        System.out.println("**** RETURNING Prefs: " + portletPreferences);        Enumeration e = portletPreferences.getNames();        while (e.hasMoreElements())        {            String name = (String)e.nextElement();            System.out.println("PREF: name == " + name);        }        System.out.println("**** DONE PREF");                return portletPreferences; 
+            portletPreferences = PortletObjectAccess.getPortletPreferences(
+                org.apache.pluto.Constants.METHOD_RENDER,
+                super.getInternalPortletWindow().getPortletEntity()
+            );
+        }
+        if(log.isDebugEnabled()) {
+            log.debug("Returning Preferences: " + portletPreferences);
+            Enumeration e = portletPreferences.getNames();
+            while (e.hasMoreElements()) {
+                String name = (String)e.nextElement();
+                log.debug(" - Preference: name = " + name);
+            }
+        }
+
+        return portletPreferences; 
     }
     // --------------------------------------------------------------------------------------------
 }
