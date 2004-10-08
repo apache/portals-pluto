dfc03d3f15066f09b46b109b2229cf15a46dc117
David H. DeWolf <ddewolf@apache.org>
2004-10-08 05:51:34 +0000

    Initial Import of Pluto 1.1 seed code.
    git-svn-id: https://svn.apache.org/repos/asf/portals/pluto/branches/binding@54049 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/pluto/binding/impl/digester/DigesterPortletInfoDD.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2003,2004 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.pluto.binding.impl.digester;
+
+import org.apache.pluto.binding.PortletInfoDD;
+
+/**
+ * @author <A href="mailto:ddewolf@apache.org">David H. DeWolf</A>
+ * @version 1.0
+ * @since Mar 11, 2004 at 4:34:11 PM
+ */
+public class DigesterPortletInfoDD implements PortletInfoDD {
+
+    private String title;
+    private String shortTitle;
+    private String keywords;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getShortTitle() {
+        return shortTitle;
+    }
+
+    public void setShortTitle(String shortTitle) {
+        this.shortTitle = shortTitle;
+    }
+
+    public String getKeywords() {
+        return keywords;
+    }
+
+    public void setKeywords(String keywords) {
+        this.keywords = keywords;
+    }
+}
