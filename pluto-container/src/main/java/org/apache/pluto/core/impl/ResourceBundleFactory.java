870e8e82752bf5a3cbb417b70f783e5179122351
David H. DeWolf <ddewolf@apache.org>
2005-07-31 01:40:12 +0000

    Resolving PLUTO-146: Thank you ZHENG Zhong. Ensuring that PortletResourceBundles do not throw null pointers if the bundle and the info fields are null.
    git-svn-id: https://svn.apache.org/repos/asf/portals/pluto/branches/pluto-1.1@226600 13f79535-47bb-0310-9956-ffa450edef68

--- pluto-container/src/main/java/org/apache/pluto/core/impl/ResourceBundleFactory.java
+++ pluto-container/src/main/java/org/apache/pluto/core/impl/ResourceBundleFactory.java
@@ -17,50 +17,76 @@ package org.apache.pluto.core.impl;
 
 import org.apache.pluto.descriptors.portlet.PortletDD;
 import org.apache.pluto.descriptors.portlet.PortletInfoDD;
+import org.apache.pluto.util.StringManager;
+import org.apache.commons.logging.LogFactory;
+import org.apache.commons.logging.Log;
 
 import java.util.*;
 
 /**
+ * Factory object used to create Portlet Resource Bundles.
+ *
  * @author <a href="ddewolf@apache.org">David H. DeWolf</a>
+ *
  */
 class ResourceBundleFactory {
     
-    private static final ResourceBundle EMPTY_BUNDLE =
-        new ListResourceBundle() {
-            private String[][] contents = new String[][] {};
-            public Object[][] getContents() {
-                return contents;
-            }
-        };
+    private static final Log LOG = 
+        LogFactory.getLog(ResourceBundleFactory.class);
+    
+    private static final StringManager EXCEPTIONS =
+        StringManager.getManager(ResourceBundleFactory.class.getPackage().getName());
 
+    /**
+     * The String value utilized for missing keys.
+     * Specifically, whenever <code>javax.portlet.title</code>,
+     * <code>javax.portlet.shorttitle</code>, or <code>javax.portlet.keywords</code>
+     * are null, this value will be returned.
+     */
+    private static final String NA = "N/A";
+
+    /**
+     * The default resource bundle which is utilized when
+     * the info is not set in the PortletInfo OR in the
+     * actual resource bundle.
+     */
+    private static final ResourceBundle EMPTY_BUNDLE = createDefaultBundle(null, null, null);
+
+    /**
+     * The default (no local) resources bundle for
+     * this bundle.
+     */
     private ResourceBundle defaultBundle;
+
+    /**
+     * All of the previously loaded bundles.
+     */
     private Map bundles = new java.util.HashMap();
+
+    /**
+     * The name of the bundle.
+     */
     private String bundleName;
 
     public ResourceBundleFactory(PortletDD dd) {
+        bundleName = dd.getResourceBundle();
+
         PortletInfoDD info = dd.getPortletInfo();
         if(info != null) {
-            final String[] title = new String[]{"javax.portlet.title",
-                                                info.getTitle()};
-            final String[] shrtt = new String[]{"javax.portlet.short-title",
-                                                info.getShortTitle()};
-            final String[] keys = new String[]{"javax.portlet.keywords",
-                                               info.getKeywords()};
-            defaultBundle = new ListResourceBundle() {
-                Object[][] contents =  new String[][]{title, shrtt, keys};
-                public Object[][] getContents() {
-                    return contents;
-                }
-            }; 
+            defaultBundle = createDefaultBundle(
+                    info.getTitle(),
+                    info.getShortTitle(),
+                    info.getKeywords()
+            );
         }
         else {
             defaultBundle = EMPTY_BUNDLE;
         }
-
-        bundleName = dd.getResourceBundle();
     }
 
     public ResourceBundle getResourceBundle(Locale locale) {
+
+        // If allready loaded for this local, return immediately!
         if (bundles.containsKey(locale)) {
             return (ResourceBundle) bundles.get(locale);
         }
@@ -78,10 +104,32 @@ class ResourceBundleFactory {
                 }
             }
         } catch (MissingResourceException mre) {
+            LOG.info(
+                EXCEPTIONS.getString("warning.resourcebundle.notfound",bundleName, mre.getMessage())
+            );
             // intentionally swallow.  Allow the default Bundle.
         }
 
         bundles.put(locale, defaultBundle);
+        return defaultBundle;
+    }
+
+    private static ResourceBundle createDefaultBundle(String title, String shortTitle, String keywords) {
+        String titleValue = title == null ? NA : title;
+        String shortValue = shortTitle == null ? NA : shortTitle;
+        String keysValue = keywords == null ? NA : keywords;
+
+        final String[] a = new String[] {"javax.portlet.title", titleValue};
+        final String[] b = new String[] {"javax.portlet.shorttitle", shortValue};
+        final String[] c = new String[] {"javax.portlet.keywords", keysValue };
+
+        ResourceBundle defaultBundle = new ListResourceBundle() {
+            Object[][] contents =  new String[][]{a, b, c};
+            public Object[][] getContents() {
+                return contents;
+            }
+        };
+
         return defaultBundle;
     }
 }
