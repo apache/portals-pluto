98ae301ee30928784c245b8190ee5a19e6304de4
David H. DeWolf <ddewolf@apache.org>
2005-10-27 15:18:03 +0000

    Finishing refactoring.  Adding new services
    git-svn-id: https://svn.apache.org/repos/asf/portals/pluto/branches/pluto-1.1@328893 13f79535-47bb-0310-9956-ffa450edef68

---
+++ pluto-container/src/main/java/org/apache/pluto/services/OptionalPortletContainerServices.java
@@ -0,0 +1,16 @@
+package org.apache.pluto.services;
+
+import javax.portlet.PortalContext;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Defines the services necessary for integration between the Pluto Container
+ * and a Portal.
+ * @author <a href="ddewolf@apache.org">David H. DeWolf</a>
+ */
+public interface OptionalPortletContainerServices {
+
+    PortletPreferencesService getPortletPreferencesService();
+
+
+}
