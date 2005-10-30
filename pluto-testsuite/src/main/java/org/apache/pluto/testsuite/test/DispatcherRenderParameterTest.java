c0f7dfcbdfdd5f7c2654c90efaa6509552fb7ec5
Zheng Zhong <zheng@apache.org>
2005-10-30 22:38:42 +0000

    Merged DispatcherRenderparameterTestServlet into DispatcherRenderParameterTest as a nested static class (TargetServlet).
    git-svn-id: https://svn.apache.org/repos/asf/portals/pluto/branches/pluto-1.1@329663 13f79535-47bb-0310-9956-ffa450edef68

--- pluto-testsuite/src/main/java/org/apache/pluto/testsuite/test/DispatcherRenderParameterTest.java
+++ pluto-testsuite/src/main/java/org/apache/pluto/testsuite/test/DispatcherRenderParameterTest.java
@@ -26,21 +26,27 @@ import javax.portlet.PortletRequestDispatcher;
 import javax.portlet.PortletResponse;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 
 /**
- * <B>TODO</B>: Document
- * @author <a href="ddewolf@apache.org">David H. DeWolf</a>
+ * 
+ * @author <a href="mailto:ddewolf@apache.org">David H. DeWolf</a>
+ * @author <a href="mailto:zheng@apache.org">ZHENG Zhong</a>
  * @version 1.0
  * @since Mar 9, 2005
  */
 public class DispatcherRenderParameterTest 
-    extends AbstractReflectivePortletTest  {
+extends AbstractReflectivePortletTest {
 
     public static final String KEY_A = "includedTestKey";
     public static final String VAL_A = "includedTestVal";
     public static final String KEY_B = "bTestKey";
     public static final String VAL_B = "bTestVal";
-    public static final String RESULT_KEY = "org.apache.pluto.testsuite.RESULTKEY";
+    public static final String RESULT_KEY =
+    	DispatcherRenderParameterTest.class.getName() + ".RESULT_KEY";
     
 
     public DispatcherRenderParameterTest() {
@@ -51,28 +57,58 @@ public class DispatcherRenderParameterTest
         return "Dispatcher Render Parameter Test";
     }
     
-    protected TestResult checkIncludedParameterTest(PortletContext ctx,
-                                                    PortletRequest req,
-                                                    PortletResponse res)
+    protected TestResult checkIncludedParameterTest(PortletContext context,
+                                                    PortletRequest request,
+                                                    PortletResponse response)
     throws IOException, PortletException {
-        PortletRequestDispatcher disp =
-            ctx.getRequestDispatcher("/tests/include?"+KEY_A+"="+VAL_A+"&"+KEY_B+"="+VAL_B);
-        
-        disp.include((RenderRequest)req, (RenderResponse)res);
+    	StringBuffer buffer = new StringBuffer();
+    	buffer.append("/tests/include?")
+    			.append(KEY_A).append("=").append(VAL_A).append("&")
+    			.append(KEY_B).append("=").append(VAL_B);
+        PortletRequestDispatcher dispatcher = context.getRequestDispatcher(
+        		buffer.toString());
+        dispatcher.include((RenderRequest) request, (RenderResponse) response);
 
         TestResult result = new TestResult();
         result.setName("Dispatcher Render Parameter Test");
         result.setDesc("Ensure query parameters added during dispatching are attached to the request.");
-        if(req.getAttribute(RESULT_KEY)!=null && Boolean.TRUE.equals(req.getAttribute(RESULT_KEY))) {
+        if (request.getAttribute(RESULT_KEY) != null
+        		&& Boolean.TRUE.equals(request.getAttribute(RESULT_KEY))) {
             result.setReturnCode(TestResult.PASSED);
-        }
-        else {
+        } else {
             result.setReturnCode(TestResult.FAILED);
             result.setResults("Unable to find expected parameter");
         }
         return result;
     }
     
+    
+    // Nested Servlet Class ----------------------------------------------------
+    
+    /**
+     * Nested static servlet class which is the target of dispatching.
+     * @author <a href="mailto:ddewolf@apache.org">David H. DeWolf</a>
+     * @author <a href="mailto:zheng@apache.org">ZHENG Zhong</a>
+     */
+    public static class TargetServlet extends GenericServlet {
 
+        public TargetServlet() {
+        	// Do nothing.
+        }
+
+        public void service(ServletRequest request, ServletResponse response)
+        throws ServletException, IOException {
+            String valueA = request.getParameter(KEY_A);
+            String valueB = request.getParameter(KEY_B);
+            if (VAL_A.equals(valueA) && VAL_B.equals(valueB)) {
+                request.setAttribute(RESULT_KEY, Boolean.TRUE);
+            }
+        }
+        
+        public String getServletInfo() {
+            return "Dispather Render Parameter Include Servlet";
+        }
+    }
+    
 }
 
