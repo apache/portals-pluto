78be85dcae9afb70e3adabb635de56acbb7366b8
David H. DeWolf <ddewolf@apache.org>
2005-11-17 16:20:55 +0000

    Implementing Database Creation on Startup
    git-svn-id: https://svn.apache.org/repos/asf/portals/pluto/trunk@345276 13f79535-47bb-0310-9956-ffa450edef68

--- pluto-portal-driver/src/main/java/org/apache/pluto/driver/services/container/db/EmbeddedDataSourceManager.java
+++ pluto-portal-driver/src/main/java/org/apache/pluto/driver/services/container/db/EmbeddedDataSourceManager.java
@@ -25,6 +25,13 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Iterator;
 
 
 /**
@@ -91,6 +98,63 @@ public class EmbeddedDataSourceManager implements DataSourceManager {
                 LOG.info("Pluto Database does not exist.  Creating database now.");
             }
 
+            boolean autoCommit = false;
+            Connection conn = null;
+            Statement  stmt = null;
+            try {
+                List script = readScript();
+                conn = embeddedDataSource.getConnection();
+                autoCommit = conn.getAutoCommit();
+                conn.setAutoCommit(false);
+
+                stmt = conn.createStatement();
+                Iterator it = script.iterator();
+                int results = 0;
+                while(it.hasNext()) {
+                    String sql = it.next().toString();
+                    if(sql.trim().length() > 0) {
+                       results += stmt.executeUpdate(sql);
+                    }
+                }
+                conn.commit();
+                if(LOG.isDebugEnabled()) {
+                    LOG.debug("Created "+results+ " database objects");
+                }
+           }
+            catch(IOException io) {
+                if(LOG.isErrorEnabled()) {
+                    LOG.error("Error reading script file", io);
+                }
+
+                throw new PortletContainerException(io);
+            }
+            catch(SQLException sql) {
+                try {
+                    if(conn != null)
+                        conn.rollback();
+                } catch(SQLException sqle) {
+                    if(LOG.isErrorEnabled()) {
+                        LOG.error("SQLException rolling back connection", sqle);
+                    }
+                }
+
+
+                if(LOG.isErrorEnabled()) {
+                    LOG.error("Error creating database schema", sql);
+                }
+                throw new PortletContainerException(sql);
+            }
+            finally {
+                try {
+                    conn.setAutoCommit(autoCommit);
+                }
+                catch(SQLException sql) {
+                    if(LOG.isErrorEnabled()) {
+                        LOG.error("Error resetting autocommit.",sql);
+                    }
+                }
+                cleanup(conn, stmt, null);
+            }
         }
             // 1) Read in the PORTABLE CREATE SQL Script
             // 2) Execute the CREATE Script via JDBC
@@ -109,19 +173,62 @@ public class EmbeddedDataSourceManager implements DataSourceManager {
         try {
             conn = embeddedDataSource.getConnection();
             stmt = conn.createStatement();
-            rs   = stmt.executeQuery("SELECT count(*) FROM SYS.SYSTABLES WHERE TABLENAME = 'PPD_VERSION'");
+            rs   = stmt.executeQuery("SELECT count(*) FROM SYS.SYSTABLES WHERE TABLENAME = 'VERSION'");
             if(rs.next()) {
                 found = rs.getInt(1) == 1;
             }
             else throw new SQLException("No results returned.  Should never happen for count(*)");
         }
         catch(SQLException sql) {
+            if(LOG.isErrorEnabled()) {
+                LOG.error("Error testing database validity", sql);
+            }
             throw new PortletContainerException(sql);
         }
         finally {
             cleanup(conn, stmt, rs);
         }
        return found;
+    }
+
+    private ArrayList readScript() throws IOException {
+        InputStream in = getClass().getResourceAsStream("/create_schema.sql");
+        if(in == null) {
+            throw new IOException("Unable to find schemal data definition");
+        }
+
+        BufferedReader input = new BufferedReader(new InputStreamReader(in));
+
+        ArrayList commands = new ArrayList();
+        StringBuffer sb = new StringBuffer();
+        String read;
+
+        if(LOG.isDebugEnabled()) {
+            LOG.debug("Adding SQL Commands for schema creation");
+        }
+
+        while( (read = input.readLine()) != null) {
+            read = read.trim();
+            if(read.startsWith("--"))
+                continue;
+
+           if(read.indexOf(';') > -0) {
+                read = read.replace(';', ' ');
+                if(LOG.isDebugEnabled()) {
+                    LOG.debug(sb.toString());
+                }
+                commands.add(sb.append(read).toString());
+                sb = new StringBuffer();
+               continue;
+            }
+
+            if(read.length() > 0) {
+                sb.append(read).append(" ");
+           }
+        }
+
+        input.close();
+        return commands;
     }
 
     private void cleanup(Connection conn, Statement stmt, ResultSet rs) {
