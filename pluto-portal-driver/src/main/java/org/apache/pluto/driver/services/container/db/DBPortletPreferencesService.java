d8053af4aa2fc51e10e49cca4cbdbdbf3ec4945f
Craig Doremus <cdoremus@apache.org>
2005-11-23 17:26:33 +0000

    Implemented getPortletPreferences() and storePreferencs() using DerbyPlutoContainerDAO. Commented out other code for now.
    git-svn-id: https://svn.apache.org/repos/asf/portals/pluto/trunk@348498 13f79535-47bb-0310-9956-ffa450edef68

--- pluto-portal-driver/src/main/java/org/apache/pluto/driver/services/container/db/DBPortletPreferencesService.java
+++ pluto-portal-driver/src/main/java/org/apache/pluto/driver/services/container/db/DBPortletPreferencesService.java
@@ -15,7 +15,7 @@
  */
 package org.apache.pluto.driver.services.container.db;
 
-import java.sql.Connection;
+//import java.sql.Connection;
 import java.sql.SQLException;
 
 import javax.portlet.PortletRequest;
@@ -23,6 +23,7 @@ import javax.portlet.PortletRequest;
 import org.apache.pluto.PortletWindow;
 import org.apache.pluto.PortletContainerException;
 import org.apache.pluto.core.PortletPreference;
+import org.apache.pluto.derby.DerbyPlutoContainerDAO;
 import org.apache.pluto.services.optional.PortletPreferencesService;
 import org.apache.commons.logging.LogFactory;
 import org.apache.commons.logging.Log;
@@ -31,6 +32,7 @@ import org.apache.commons.logging.Log;
  * The Portal Driver's PortletPreferencesService implementation. This
  * implementation makes use of the embeded derby db for writing portlet preferences.
  * @author <a href="ddewolf@apache.org">David H. DeWolf</a>
+ * @author <a href="cdoremus@apache.org">Craig Doremus</a>
  * @version 1.0
  * @since Sep 22, 2004
  */
@@ -39,7 +41,12 @@ public class DBPortletPreferencesService
 
     private static final Log LOG =
         LogFactory.getLog(DBPortletPreferencesService.class);
-
+    
+    public DBPortletPreferencesService() {
+    	
+    }
+    
+/*    
     private DataSourceManager dataSourceManager;
 
     public DBPortletPreferencesService(DataSourceManager dataSourceManager)
@@ -60,19 +67,39 @@ public class DBPortletPreferencesService
             }
         }
     }
-
+*/
+    
     public PortletPreference[] getStoredPreferences(PortletWindow window,
                                                     PortletRequest req)
     throws PortletContainerException {
-
+    	PortletPreference[] prefs = null;
         try {
-            Connection conn = getConnection();
-            conn.close();
+//            Connection conn = getConnection();
+//            conn.close();
+        	if (window == null) {
+        		throw new PortletContainerException("Null PortletWindow");
+        	}
+        	String user = req.getRemoteUser();
+        	if (user == null) {
+        		user = "ANONYMOUS";
+        	}
+        	String context = window.getContextPath();
+        	String portlet = window.getPortletName();
+        	if (LOG.isDebugEnabled()) {
+        		LOG.debug("Retrieving preferences for user '" + user +
+        				"' at context '" + context +
+        				"' for portlet '" + portlet + "'");
+        	}
+        	prefs = DerbyPlutoContainerDAO.getStoredPreferences(
+        				context, 
+        				portlet, 
+        				user);
         }
-        catch(SQLException sql) {
-            throw new PortletContainerException(sql);
+        catch(SQLException e) {
+        	LOG.error(e);
+            throw new PortletContainerException(e);
         }
-        return new PortletPreference[0];
+        return prefs;
     }
 
     public void store(PortletWindow window,
@@ -80,17 +107,43 @@ public class DBPortletPreferencesService
                       PortletPreference[] preferences)
         throws PortletContainerException {
         try {
-            Connection conn = getConnection();
-            conn.close();
+//            Connection conn = getConnection();
+//            conn.close();
+        	if (window == null) {
+        		throw new PortletContainerException("Null PortletWindow");
+        	}
+        	if (preferences == null) {
+        		throw new PortletContainerException("Null PortletPreference array");
+        	}
+        	if (preferences.length == 0) {
+        		LOG.warn("No preferences to store!");
+        	}
+        	String user = request.getRemoteUser();
+        	if (user == null) {
+        		user = "ANONYMOUS";
+        	}
+        	String context = window.getContextPath();
+        	String portlet = window.getPortletName();
+        	if (LOG.isDebugEnabled()) {
+        		LOG.debug("Retrieving preferences for user '" + user +
+        				"' at context '" + context +
+        				"' for portlet '" + portlet + "'");
+        	}
+        	DerbyPlutoContainerDAO.storePreferences(
+        			context, 
+        			portlet, 
+        			preferences, 
+        			user);
         }
-        catch(SQLException sql) {
-            throw new PortletContainerException(sql);
+        catch(SQLException e) {
+        	LOG.error(e);
+            throw new PortletContainerException(e);
         }
     }
 
-    private Connection getConnection() throws SQLException {
-        return dataSourceManager.getPortalDataSource().getConnection();
-    }
+//    private Connection getConnection() throws SQLException {
+//        return dataSourceManager.getPortalDataSource().getConnection();
+//    }
 
 }
 
