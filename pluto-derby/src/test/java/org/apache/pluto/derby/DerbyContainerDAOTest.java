9150ce5cea33611ba72841bc71d7b30ad88dee0a
Craig Doremus <cdoremus@apache.org>
2005-11-23 00:04:10 +0000

    Added test for getStoredPreferences() and doInsert() with returned generated ID and transactions.
    git-svn-id: https://svn.apache.org/repos/asf/portals/pluto/trunk@348306 13f79535-47bb-0310-9956-ffa450edef68

--- pluto-derby/src/test/java/org/apache/pluto/derby/DerbyContainerDAOTest.java
+++ pluto-derby/src/test/java/org/apache/pluto/derby/DerbyContainerDAOTest.java
@@ -2,6 +2,8 @@ package org.apache.pluto.derby;
 
 import java.io.File;
 import java.sql.SQLException;
+
+import org.apache.pluto.core.PortletPreference;
 import org.apache.pluto.derby.DerbyPlutoContainerDAO;
 
 import junit.framework.TestCase;
@@ -38,6 +40,7 @@ public class DerbyContainerDAOTest extends TestCase {
 			System.out.println("Portlet App ID of context '" + context + "' = " + id);
 			assertTrue(id == 1);
 		} catch (SQLException e) {
+			e.printStackTrace();
 			fail(e.toString());
 		}
 	}
@@ -51,6 +54,7 @@ public class DerbyContainerDAOTest extends TestCase {
 			System.out.println("ID of portlet '" + portletName + "' at app context '" + context + "' = " + id);
 			assertTrue(id == 1);
 		} catch (SQLException e) {
+			e.printStackTrace();
 			fail(e.toString());
 		}
 	}
@@ -59,14 +63,114 @@ public class DerbyContainerDAOTest extends TestCase {
 		String context = "/testsuite";
 		String portletName = "TestPortlet1";
 		String preferenceName = "dummyName";
+		String authUser = "ANONYMOUS";
 		int id = 0;
 		try {
-			id = DerbyPlutoContainerDAO.findPreferenceId(context, portletName, preferenceName);
+			id = DerbyPlutoContainerDAO.findPreferenceId(context, portletName, preferenceName, authUser);
 			System.out.println("ID of preference '" + preferenceName + "' in portlet '" + portletName + "' at app context '" + context + "' = " + id);
 			assertTrue(id == 1);
 		} catch (SQLException e) {
+			e.printStackTrace();
 			fail(e.toString());
 		}
 	}
 	
+	 public void testGetStoredPreferences(){
+		 String context = "/testsuite";
+		String portletName = "TestPortlet1";
+		String authUser = "ANONYMOUS";
+			try {
+				PortletPreference[] prefs = DerbyPlutoContainerDAO.getStoredPreferences(context, portletName, authUser);
+				assertNotNull(prefs);
+				assertTrue(prefs.length > 0);
+				for (int i = 0; i < prefs.length; i++) {
+					PortletPreference pref = (PortletPreference)prefs[i];
+					if (i == 0) {
+						assertTrue(pref.getName().equals("dummyName"));
+						assertTrue(pref.getValues()[0].equals("dummyValue"));
+						assertTrue(pref.isReadOnly() == false);						
+					}
+					if (i == 1) {
+						assertTrue(pref.getName().equals("dummyName2"));
+						assertTrue(pref.getValues()[0].equals("dummyValue2"));
+						assertTrue(pref.isReadOnly() == false);						
+					}
+					if (i == 2) {
+						assertTrue(pref.getName().equals("readonly"));
+						assertTrue(pref.getValues()[0].equals("readonly"));
+						assertTrue(pref.isReadOnly() == true);						
+					}
+//					System.out.println("Preference name:" +  pref.getName());
+//					String val0 = pref.getValues()[0];
+//					System.out.println("Preference value:" +  val0);
+//					System.out.println("Read only:" + pref.isReadOnly());
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+				fail(e.toString());
+			}
+		 
+	 }
+
+	 public void testInsertTransaction() {
+		 //Do this with explicit transaction
+		 DerbyDataStore ds = new DerbyDataStore(false);
+		 int id = 0;
+		 int id2 = 0;
+		 int pid = 0;
+		 int pid2 = 0;
+		 
+		 final String[] testContext = {"/test"};
+		 final String[] testContext2 = {"/test2"};
+		 final String testName = "TestPortlet";
+		 final String testName2 = "TestPortlet2";
+		 
+		try {
+			id = ds.doInsert("insert into container.portlet_app (app_context) values (?)", testContext);
+			 assertTrue(id > 0);
+			System.out.println("First Generated ID = " + id);
+			id2 = ds.doInsert("insert into container.portlet_app (app_context) values (?)", testContext2);
+			 assertTrue(id2  > 0);
+			System.out.println("Second Generated ID = " + id2);
+			 assertTrue(id2 - id == 1);
+			Object[] params1 = new Object[]{
+						new Integer(id),
+						new String(testName)
+						}; 
+			pid = ds.doInsert("insert into container.portlet (portlet_app_id, portlet_name) values (?, ?)", params1);
+			assertTrue(pid > 0);
+			System.out.println("First Portlet Generated ID = " + pid);
+			Object[] params2 = new Object[]{
+					new Integer(id2),
+					new String(testName2)
+					}; 
+			pid2 = ds.doInsert("insert into container.portlet (portlet_app_id, portlet_name) values (?, ?)", params2);
+			assertTrue(pid2 > 0);
+   		    assertTrue(pid2 - pid == 1);
+   		    ds.commit();
+			 
+		} catch (SQLException e) {
+			ds.rollback();
+			e.printStackTrace();
+			fail(e.toString());
+		} finally {
+			if (ds != null) {
+				try {
+					ds.doDelete("delete from container.portlet where portlet_id=?", pid);
+					ds.doDelete("delete from container.portlet where portlet_id=?", pid2);
+					ds.doDelete("delete from container.portlet_app where portlet_app_id=?", id);
+					ds.doDelete("delete from container.portlet_app where portlet_app_id=?", id2);
+					ds.commit();
+				} catch (SQLException e) {
+					ds.rollback();
+					e.printStackTrace();
+					fail("Fail during delete: " + e.toString());
+				}
+			}
+			
+		}
+		ds.closeConnection();
+	 }
 }
+
+
