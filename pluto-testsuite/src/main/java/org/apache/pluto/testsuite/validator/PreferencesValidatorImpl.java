80fd147d10a19d239b1ad3cd5616b1da5244a4f3
Carsten Ziegeler <cziegeler@apache.org>
2006-04-21 11:36:48 +0000

    Add some missing license headers
    git-svn-id: https://svn.apache.org/repos/asf/portals/pluto/trunk@395860 13f79535-47bb-0310-9956-ffa450edef68

--- pluto-testsuite/src/main/java/org/apache/pluto/testsuite/validator/PreferencesValidatorImpl.java
+++ pluto-testsuite/src/main/java/org/apache/pluto/testsuite/validator/PreferencesValidatorImpl.java
@@ -13,10 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/* 
-
- */
-
 package org.apache.pluto.testsuite.validator;
 
 import javax.portlet.PortletPreferences;
