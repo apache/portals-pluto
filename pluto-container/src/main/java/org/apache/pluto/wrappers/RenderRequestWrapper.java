b48d6b72f7eb65d2e8e19c178f3f766c62a24dec
Carsten Ziegeler <cziegeler@apache.org>
2006-04-21 09:57:23 +0000

    Add some missing license headers
    git-svn-id: https://svn.apache.org/repos/asf/portals/pluto/trunk@395829 13f79535-47bb-0310-9956-ffa450edef68

--- pluto-container/src/main/java/org/apache/pluto/wrappers/RenderRequestWrapper.java
+++ pluto-container/src/main/java/org/apache/pluto/wrappers/RenderRequestWrapper.java
@@ -13,10 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/* 
-
- */
-
 package org.apache.pluto.wrappers;
 
 import javax.portlet.RenderRequest;
