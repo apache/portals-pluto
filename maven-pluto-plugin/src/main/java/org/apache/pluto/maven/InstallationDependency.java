21553f51f7c46c412546c14af076889dd48db61d
David H. DeWolf <ddewolf@apache.org>
2006-08-24 02:18:29 +0000

    Updating pom versions to match convention
    git-svn-id: https://svn.apache.org/repos/asf/portals/pluto/trunk@434290 13f79535-47bb-0310-9956-ffa450edef68

--- maven-pluto-plugin/src/main/java/org/apache/pluto/maven/InstallationDependency.java
+++ maven-pluto-plugin/src/main/java/org/apache/pluto/maven/InstallationDependency.java
@@ -25,7 +25,7 @@ import java.util.Collection;
  */
 class InstallationDependency {
 
-	public static final String VERSION = "1.1.0-dev";
+	public static final String VERSION = "1.1.0-SNAPSHOT";
 
     public static final InstallationDependency PORTLET_API =
         new InstallationDependency("javax.portlet", "portlet-api", "1.0");
