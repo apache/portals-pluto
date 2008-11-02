fc09a7e5ff5be901c9fd6d0a93e9a5cda1467f2c
Ate Douma <ate@apache.org>
2008-11-02 21:47:07 +0000

    PLUTO-520: Provide backwards compatiblity for Portlet Applications using/embedding Pluto 1.0.1 portlet.tld See: http://issues.apache.org/jira/browse/PLUTO-520
    git-svn-id: https://svn.apache.org/repos/asf/portals/pluto/branches/2.0-spi-refactoring@709944 13f79535-47bb-0310-9956-ffa450edef68

---
+++ pluto-taglib/src/main/java/org/apache/pluto/tags/ActionURLTag.java
@@ -0,0 +1,35 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.pluto.tags;
+
+/**
+ * Backwards compatibility Pluto 1.0.1 ActionURLTag providing support for old
+ * Pluto 1.0.1 portlet.tld usages.
+ * <p>
+ * Although a portlet.tld should not be provided by Portlet Applications
+ * themselves but "injected" by the target portlet container, fact is many
+ * have them embedded in the application anyway. This class ensures those
+ * applications still can use their old portlet.tld.
+ * </p>
+ * @version $Id$
+ * @deprecated
+ */
+public class ActionURLTag extends ActionURLTag168
+{
+    private static final long serialVersionUID = 286L;
+}
