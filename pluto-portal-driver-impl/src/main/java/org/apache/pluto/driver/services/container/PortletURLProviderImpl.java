e2d6d59745d36dd0a57922331d63c80ee7116b89
Ate Douma <ate@apache.org>
2008-11-19 16:21:37 +0000

    PLUTO-523: ensure included requests see at least the original render parameters
    git-svn-id: https://svn.apache.org/repos/asf/portals/pluto/trunk@719006 13f79535-47bb-0310-9956-ffa450edef68

--- pluto-portal-driver-impl/src/main/java/org/apache/pluto/driver/services/container/PortletURLProviderImpl.java
+++ pluto-portal-driver-impl/src/main/java/org/apache/pluto/driver/services/container/PortletURLProviderImpl.java
@@ -156,10 +156,10 @@ public class PortletURLProviderImpl implements PortletURLProvider {
 
     public Map<String, String[]> parseRenderParameters(Map<String, String[]> parentMap, String queryString)
     {
+        // Copy all the original render parameters.
+        parameters = new HashMap<String, String[]>(parentMap);        
         if (queryString != null && queryString.trim().length() > 0)
         {
-            // Copy all the original render parameters.
-            parameters = new HashMap(parentMap);
             // Merge the appended parameters to the render parameter map.
             // The original render parameters should not be overwritten.
             mergeQueryString(parameters, queryString);
