c444968728068a20c008b6384e7638255b866ec2
Ate Douma <ate@apache.org>
2009-03-11 15:32:49 +0000

    PLUTO-540: New PortletURLListenerService See: https://issues.apache.org/jira/browse/PLUTO-538 - implemented
    git-svn-id: https://svn.apache.org/repos/asf/portals/pluto/trunk@752473 13f79535-47bb-0310-9956-ffa450edef68

---
+++ pluto-portal-driver-impl/src/main/java/org/apache/pluto/driver/services/container/PortletURLListenerServiceImpl.java
@@ -0,0 +1,65 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.pluto.driver.services.container;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.portlet.PortletURLGenerationListener;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.pluto.container.PortletURLListenerService;
+import org.apache.pluto.container.om.portlet.Listener;
+import org.apache.pluto.container.om.portlet.PortletApplicationDefinition;
+
+public class PortletURLListenerServiceImpl implements PortletURLListenerService
+{
+	/** Logger. */
+    private static final Log LOG = LogFactory.getLog(PortletURLListenerServiceImpl.class);
+
+    public List<PortletURLGenerationListener> getPortletURLGenerationListeners(PortletApplicationDefinition app)
+    {
+        List<PortletURLGenerationListener> listeners = new ArrayList<PortletURLGenerationListener>();
+        //this list is needed for the classnames
+        List<? extends Listener> portletURLFilterList = app.getListeners();
+        //Iterate over the classnames and for each entry in the list the filter..URL is called.
+        if (portletURLFilterList != null){
+            for (Listener listener : portletURLFilterList) {
+                ClassLoader loader = Thread.currentThread().getContextClassLoader();
+                Class<? extends Object> clazz;
+                try {
+                    clazz = loader.loadClass(listener.getListenerClass() );
+                    if (clazz != null){
+                        listeners.add((PortletURLGenerationListener)clazz.newInstance());
+                    }
+                } catch (ClassNotFoundException e) {
+                    String message = "The class isn't found.";
+                    LOG.error(message);
+                } catch (InstantiationException e) {
+                    String message = "The instantiation fail.";
+                    LOG.error(message);
+                } catch (IllegalAccessException e) {
+                    String message = "IllegalAccessException";
+                    LOG.error(message);
+                }
+            }
+        }
+        return listeners;
+    }
+}
