db941c8775b192f53ff58e7ee8585cade3c88517
Eric Dalquist <edalquist@apache.org>
2010-10-25 02:01:29 +0000

    Partially reverting r1026934 to remove accidentally committed files
    git-svn-id: https://svn.apache.org/repos/asf/portals/pluto/trunk@1026935 13f79535-47bb-0310-9956-ffa450edef68

--- installPluto.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-mvn org.apache.portals.pluto:maven-pluto-plugin:2.1.0-SNAPSHOT:install -DinstallDir=/Users/edalquist/java/tomcat/apache-tomcat-pluto
--- tckDeploy.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-find /Users/edalquist/tmp/webapps/ -name '*.war' -exec ant -f tck-deploy.xml deployPortletApp -DportletApp={} -DdestDir=/Users/edalquist/java/tomcat/apache-tomcat-pluto/webapps \;
