a8272f83454ae0432a7f923649ebdcd96b340e55
Scott Nicklous <msnicklous@apache.org>
2014-08-14 15:03:02 +0200

    Added generated test framework code for basic dispatcher tests that need to be developed manually.

---
+++ portlet-tck_3.0/V2DispatcherTests3S/src/main/java/javax/portlet/tck/servlets/DispatcherTests3S_SPEC2_19_IncludeServletAction_servlet.java
@@ -0,0 +1,106 @@
+/*  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+
+
+package javax.portlet.tck.servlets;
+
+import java.io.*;
+import java.util.*;
+import java.util.logging.*;
+import static java.util.logging.Logger.*;
+import javax.portlet.*;
+import javax.portlet.filter.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.portlet.tck.beans.*;
+import javax.portlet.tck.constants.*;
+import static javax.portlet.tck.constants.Constants.*;
+import static javax.portlet.tck.beans.JSR286DispatcherTestCaseDetails.*;
+
+/**
+ * Servlet for JSR 362 request dispatcher testing.
+ * Used by portlet: DispatcherTests3S_SPEC2_19_IncludeServletAction
+ *
+ * @author nick
+ *
+ */
+public class DispatcherTests3S_SPEC2_19_IncludeServletAction_servlet extends HttpServlet {
+   private static final String LOG_CLASS = 
+         DispatcherTests3S_SPEC2_19_IncludeServletAction_servlet.class.getName();
+   private final Logger LOGGER = Logger.getLogger(LOG_CLASS);
+
+   @Override
+   protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+         throws ServletException, IOException {
+      processTCKReq(req, resp);
+   }
+
+   @Override
+   protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+         throws ServletException, IOException {
+      processTCKReq(req, resp);
+   }
+
+   // The tck uses only get & post requests
+   protected void processTCKReq(HttpServletRequest request, HttpServletResponse response)
+         throws ServletException, IOException {
+      LOGGER.entering(LOG_CLASS, "servlet entry");
+
+      PortletRequest portletReq = (PortletRequest) request.getAttribute("javax.portlet.request");
+      PortletResponse portletResp = (PortletResponse) request.getAttribute("javax.portlet.response");
+      PortletConfig portletConfig = (PortletConfig) request.getAttribute("javax.portlet.config");
+      long svtTid = Thread.currentThread().getId();
+      long reqTid = (Long) portletReq.getAttribute("void");
+
+      StringWriter writer = new StringWriter();
+
+      JSR286DispatcherTestCaseDetails tcd = new JSR286DispatcherTestCaseDetails();
+
+      // Create result objects for the tests
+
+      /* TestCase: V2DispatcherTests3S_SPEC2_19_IncludeServletAction_dispatch4 */
+      /* Details: "The parameters associated with a request dispatcher are    */
+      /* scoped only for the duration of the include or forward call"         */
+      TestResult tr0 = tcd.getTestResultFailed(V2DISPATCHERTESTS3S_SPEC2_19_INCLUDESERVLETACTION_DISPATCH4);
+      /* TODO: implement test */
+      tr0.appendTcDetail("Not implemented.");
+      tr0.writeTo(writer);
+
+      /* TestCase: V2DispatcherTests3S_SPEC2_19_IncludeServletAction_invoke3  */
+      /* Details: "Parameters to the include method for a target servlet      */
+      /* can be wrapped request and response classes from the portlet         */
+      /* lifecyle method initiating the include"                              */
+      TestResult tr1 = tcd.getTestResultFailed(V2DISPATCHERTESTS3S_SPEC2_19_INCLUDESERVLETACTION_INVOKE3);
+      /* TODO: implement test */
+      tr1.appendTcDetail("Not implemented.");
+      tr1.writeTo(writer);
+
+      /* TestCase: V2DispatcherTests3S_SPEC2_19_IncludeServletAction_invoke8  */
+      /* Details: "The portlet can include multiple servlets during the       */
+      /* same lifecycle method"                                               */
+      TestResult tr2 = tcd.getTestResultFailed(V2DISPATCHERTESTS3S_SPEC2_19_INCLUDESERVLETACTION_INVOKE8);
+      /* TODO: implement test */
+      tr2.appendTcDetail("Not implemented.");
+      tr2.writeTo(writer);
+
+      request.getSession().setAttribute(
+                   Constants.RESULT_ATTR_PREFIX + "DispatcherTests3S_SPEC2_19_IncludeServletAction",
+                   writer.toString());
+
+   }
+}
---
+++ portlet-tck_3.0/V2DispatcherTests3S/src/main/webapp/WEB-INF/jsp/DispatcherTests3S_SPEC2_19_IncludeJSPRender.jsp
@@ -0,0 +1,50 @@
+<%@ page session="false" %>
+<%@ taglib uri="http://java.sun.com/portlet_2_0"  prefix="portlet" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page import="java.io.*,java.util.*,java.util.logging.*" %>
+<%@ page import="static java.util.logging.Logger.*" %>
+<%@ page import="javax.portlet.*,javax.portlet.filter.*" %>
+<%@ page import="javax.servlet.http.*,javax.servlet.*" %>
+<%@ page import="javax.portlet.tck.beans.*,javax.portlet.tck.constants.*" %>
+<%@ page import="static javax.portlet.tck.constants.Constants.*" %>
+<%@ page import="static javax.portlet.tck.beans.JSR286DispatcherTestCaseDetails.*" %>
+<%
+
+      PortletRequest portletReq = (PortletRequest) request.getAttribute("javax.portlet.request");
+      PortletResponse portletResp = (PortletResponse) request.getAttribute("javax.portlet.response");
+      PortletConfig portletConfig = (PortletConfig) request.getAttribute("javax.portlet.config");
+      long svtTid = Thread.currentThread().getId();
+      long reqTid = (Long) portletReq.getAttribute("void");
+
+      PrintWriter writer = ((MimeResponse)portletResp).getWriter();
+
+      JSR286DispatcherTestCaseDetails tcd = new JSR286DispatcherTestCaseDetails();
+
+      // Create result objects for the tests
+
+      /* TestCase: V2DispatcherTests3S_SPEC2_19_IncludeJSPRender_dispatch4    */
+      /* Details: "The parameters associated with a request dispatcher are    */
+      /* scoped only for the duration of the include or forward call"         */
+      TestResult tr0 = tcd.getTestResultFailed(V2DISPATCHERTESTS3S_SPEC2_19_INCLUDEJSPRENDER_DISPATCH4);
+      /* TODO: implement test */
+      tr0.appendTcDetail("Not implemented.");
+      tr0.writeTo(writer);
+
+      /* TestCase: V2DispatcherTests3S_SPEC2_19_IncludeJSPRender_invoke3      */
+      /* Details: "Parameters to the include method for a target servlet      */
+      /* can be wrapped request and response classes from the portlet         */
+      /* lifecyle method initiating the include"                              */
+      TestResult tr1 = tcd.getTestResultFailed(V2DISPATCHERTESTS3S_SPEC2_19_INCLUDEJSPRENDER_INVOKE3);
+      /* TODO: implement test */
+      tr1.appendTcDetail("Not implemented.");
+      tr1.writeTo(writer);
+
+      /* TestCase: V2DispatcherTests3S_SPEC2_19_IncludeJSPRender_invoke8      */
+      /* Details: "The portlet can include multiple servlets during the       */
+      /* same lifecycle method"                                               */
+      TestResult tr2 = tcd.getTestResultFailed(V2DISPATCHERTESTS3S_SPEC2_19_INCLUDEJSPRENDER_INVOKE8);
+      /* TODO: implement test */
+      tr2.appendTcDetail("Not implemented.");
+      tr2.writeTo(writer);
+
+%>
