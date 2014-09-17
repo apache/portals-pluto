d748396f1c2a405eadc7f3c8db9e1b4d0297183b
Scott Nicklous <msnicklous@apache.org>
2014-09-17 11:25:59 +0200

    1. Added "arraysEqual" method to the CompareUtils class to check for string array equality, generate debug output if the arrays are not equal, and set the test result according to the comparison result. 2. Fixed debug output generation in the "mapsEqual" method. It was producing invalid output in the case where the keys were equal but the values were not.

--- portlet-tck_3.0/common/src/main/java/javax/portlet/tck/beans/CompareUtils.java
+++ portlet-tck_3.0/common/src/main/java/javax/portlet/tck/beans/CompareUtils.java
@@ -87,6 +87,33 @@ public class CompareUtils {
    }
 
    /**
+    * Compares two string arrays and sets the test result accordingly. 
+    * If both string arrays are null, comparison is successful.
+    * 
+    * @param asrc Source of 1st string to compare
+    * @param a    1st string array to compare
+    * @param bsrc Source of 2nd string to compare
+    * @param b    2nd string array to compare
+    * @param tr   test result to be updated
+    */
+   static public void arraysEqual(String asrc, String[] a, String bsrc, String[] b, TestResult tr) {
+      boolean ok = (a==null) && (b == null);
+      if (!ok) {
+         if ((a != null) && (b != null)) {
+            ok = Arrays.deepEquals(a, b);
+         }
+         if (!ok) {
+            String ra = (a==null) ? "null" : Arrays.asList(a).toString();
+            String rb = (b==null) ? "null" : Arrays.asList(b).toString();
+            String str = "Error: " + ra + "from " + asrc + " does not equal " + 
+                         rb + " from " + bsrc + ".";
+            tr.appendTcDetail(str);
+         }
+      }
+      tr.setTcSuccess(ok);
+   }
+
+   /**
     * Compares two Enumerations and sets the test result accordingly. 
     * If both strings are empty, comparison is successful.
     * 
@@ -147,34 +174,46 @@ public class CompareUtils {
    @SuppressWarnings({ "rawtypes", "unchecked" })
    static public void mapsEqual(String aname, Map<String, String[]> a, String bname, Map<String, String[]> b, TestResult tr) {
 
+      StringBuffer dstr = new StringBuffer(256);
+      dstr.append("<br/>a.keyset: " + a.keySet()+ "<br/>b.keyset: " + b.keySet() + "<br/>");
       boolean ok = a.keySet().equals(b.keySet());
       if (ok) {
+         dstr.append("keys compare OK: " + ok + "<br/>");
          for (String key : a.keySet()) {
             ok = ok & (Arrays.deepEquals((String[])a.get(key), (String[])b.get(key)));
+            dstr.append("Key: " + key + ", val a:" + Arrays.asList(((String[])a.get(key))) + "<br/>");
+            dstr.append("Key: " + key + ", val b:" + Arrays.asList(((String[])b.get(key))) + "<br/>");
          }
+         dstr.append("values compare OK: " + ok + "<br/>");
       }
 
       // if not OK, write debug output:
       if (!ok) {
-         StringBuffer sb = new StringBuffer(256);
-         Formatter fmtr = new Formatter(sb);
-         String str = "<br/>In %1$s but not in %2$s: <br/>\n";
-         fmtr.format(str, aname, bname);
-         HashSet diffs = new HashSet();
-         diffs.addAll(a.keySet());
-         diffs.removeAll(b.keySet());
-         for (Object key : diffs) {
-            sb.append((String)key + "<br/>\n");
+         if (!a.keySet().equals(b.keySet())) {
+            // different sets of keys
+            StringBuffer sb = new StringBuffer(256);
+            Formatter fmtr = new Formatter(sb);
+            String str = "<br/>In %1$s but not in %2$s: <br/>\n";
+            fmtr.format(str, aname, bname);
+            HashSet diffs = new HashSet();
+            diffs.addAll(a.keySet());
+            diffs.removeAll(b.keySet());
+            for (Object key : diffs) {
+               sb.append((String)key + "<br/>\n");
+            }
+            fmtr.format(str, bname, aname);
+            diffs.clear();
+            diffs.addAll(b.keySet());
+            diffs.removeAll(a.keySet());
+            for (Object key : diffs) {
+               sb.append((String)key + "<br/>\n");
+            }
+            fmtr.close();
+            tr.appendTcDetail(sb.toString());
+         } else {
+            // keys ok, but different values
+            tr.appendTcDetail(dstr.toString());
          }
-         fmtr.format(str, bname, aname);
-         diffs.clear();
-         diffs.addAll(b.keySet());
-         diffs.removeAll(a.keySet());
-         for (Object key : diffs) {
-            sb.append((String)key + "<br/>\n");
-         }
-         fmtr.close();
-         tr.appendTcDetail(sb.toString());
       }
       tr.setTcSuccess(ok);
    }
