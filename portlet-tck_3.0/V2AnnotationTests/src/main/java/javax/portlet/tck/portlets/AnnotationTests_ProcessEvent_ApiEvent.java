48c9dd2907ec5085bb12048116f78da53610171e
ahmed <mahmedk91@gmail.com>
2016-06-21 12:19:19 +0200

    Cleaned up V2AnnotationTests from extraneous logging and imports

--- portlet-tck_3.0/V2AnnotationTests/src/main/java/javax/portlet/tck/portlets/AnnotationTests_ProcessEvent_ApiEvent.java
+++ portlet-tck_3.0/V2AnnotationTests/src/main/java/javax/portlet/tck/portlets/AnnotationTests_ProcessEvent_ApiEvent.java
@@ -1,19 +1,16 @@
-/*  Licensed to the Apache Software Foundation (ASF) under one
- *  or more contributor license agreements.  See the NOTICE file
- *  distributed with this work for additional information
- *  regarding copyright ownership.  The ASF licenses this file
- *  to you under the Apache License, Version 2.0 (the
- *  "License"); you may not use this file except in compliance
- *  with the License.  You may obtain a copy of the License at
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License. You may obtain a
+ * copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing,
- *  software distributed under the License is distributed on an
- *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- *  KIND, either express or implied.  See the License for the
- *  specific language governing permissions and limitations
- *  under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 
 package javax.portlet.tck.portlets;
@@ -33,86 +30,73 @@ import javax.portlet.PortletException;
 import javax.portlet.PortletURL;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
-import javax.portlet.ResourceRequest;
-import javax.portlet.ResourceResponse;
 import javax.portlet.tck.beans.TestButton;
 import javax.xml.namespace.QName;
 
 /**
- * This portlet implements several test cases for the JSR 362 TCK. The test case names
- * are defined in the /src/main/resources/xml-resources/additionalTCs.xml
- * file. The build process will integrate the test case names defined in the 
- * additionalTCs.xml file into the complete list of test case names for execution by the driver.
+ * This portlet implements several test cases for the JSR 362 TCK. The test case names are defined
+ * in the /src/main/resources/xml-resources/additionalTCs.xml file. The build process will integrate
+ * the test case names defined in the additionalTCs.xml file into the complete list of test case
+ * names for execution by the driver.
  *
- * This is the main portlet for the test cases. If the test cases call for events, this portlet
- * will initiate the events, but not process them. The processing is done in the companion 
- * portlet AnnotationTests_ProcessEvent_ApiEvent_event
+ * This is the main portlet for the test cases. If the test cases call for events, this portlet will
+ * initiate the events, but not process them. The processing is done in the companion portlet
+ * AnnotationTests_ProcessEvent_ApiEvent_event
  *
  */
 public class AnnotationTests_ProcessEvent_ApiEvent extends GenericPortlet {
-   private static final String LOG_CLASS = 
-         AnnotationTests_ProcessEvent_ApiEvent.class.getName();
-   private final Logger LOGGER = Logger.getLogger(LOG_CLASS);
+  private static final String LOG_CLASS = AnnotationTests_ProcessEvent_ApiEvent.class.getName();
+  private final Logger LOGGER = Logger.getLogger(LOG_CLASS);
 
-   @Override
-   public void processAction(ActionRequest portletReq, ActionResponse portletResp)
-         throws PortletException, IOException {
-      LOGGER.entering(LOG_CLASS, "main portlet processAction entry");
+  @Override
+  public void processAction(ActionRequest portletReq, ActionResponse portletResp)
+      throws PortletException, IOException {
 
-      portletResp.setRenderParameters(portletReq.getParameterMap());
-      long tid = Thread.currentThread().getId();
-      portletReq.setAttribute(THREADID_ATTR, tid);
+    portletResp.setRenderParameters(portletReq.getParameterMap());
+    long tid = Thread.currentThread().getId();
+    portletReq.setAttribute(THREADID_ATTR, tid);
 
-      if (portletReq.getParameter(BUTTON_PARAM_NAME).startsWith(V2ANNOTATIONTESTS_PROCESSEVENT_APIEVENT_QNAME)) {
-         QName eventQName = new QName(TCKNAMESPACE, "AnnotationTests_ProcessEvent_ApiEvent");
-         portletResp.setEvent(eventQName, "Hi!");
-         LOGGER.finest("Firing event: " + eventQName.toString());
-      } else if (portletReq.getParameter(BUTTON_PARAM_NAME).startsWith(V2ANNOTATIONTESTS_PROCESSEVENT_APIEVENT_NAME)) {
-         portletResp.setEvent("AnnotationTests_ProcessEvent_ApiEvent2", "Hi!");
-         LOGGER.finest("Firing event: AnnotationTests_ProcessEvent_ApiEvent2");
-      }
-   }
+    if (portletReq.getParameter(BUTTON_PARAM_NAME)
+        .startsWith(V2ANNOTATIONTESTS_PROCESSEVENT_APIEVENT_QNAME)) {
+      QName eventQName = new QName(TCKNAMESPACE, "AnnotationTests_ProcessEvent_ApiEvent");
+      portletResp.setEvent(eventQName, "Hi!");
+      LOGGER.finest("Firing event: " + eventQName.toString());
+    } else if (portletReq.getParameter(BUTTON_PARAM_NAME)
+        .startsWith(V2ANNOTATIONTESTS_PROCESSEVENT_APIEVENT_NAME)) {
+      portletResp.setEvent("AnnotationTests_ProcessEvent_ApiEvent2", "Hi!");
+      LOGGER.finest("Firing event: AnnotationTests_ProcessEvent_ApiEvent2");
+    }
+  }
 
-   @Override
-   public void serveResource(ResourceRequest portletReq, ResourceResponse portletResp)
-         throws PortletException, IOException {
-      LOGGER.entering(LOG_CLASS, "main portlet serveResource entry");
+  @Override
+  public void render(RenderRequest portletReq, RenderResponse portletResp)
+      throws PortletException, IOException {
 
-      long tid = Thread.currentThread().getId();
-      portletReq.setAttribute(THREADID_ATTR, tid);
+    long tid = Thread.currentThread().getId();
+    portletReq.setAttribute(THREADID_ATTR, tid);
 
-   }
+    PrintWriter writer = portletResp.getWriter();
 
-   @Override
-   public void render(RenderRequest portletReq, RenderResponse portletResp)
-         throws PortletException, IOException {
-      LOGGER.entering(LOG_CLASS, "main portlet render entry");
+    /* TestCase: V2AnnotationTests_ProcessEvent_ApiEvent_qname */
+    /* Details: "Method qname(): On an event request, the method is */
+    /* executed if the Event qname matches the qname field" */
+    {
+      PortletURL aurl = portletResp.createActionURL();
+      aurl.setParameters(portletReq.getPrivateParameterMap());
+      TestButton tb = new TestButton(V2ANNOTATIONTESTS_PROCESSEVENT_APIEVENT_QNAME, aurl);
+      tb.writeTo(writer);
+    }
 
-      long tid = Thread.currentThread().getId();
-      portletReq.setAttribute(THREADID_ATTR, tid);
+    /* TestCase: V2AnnotationTests_ProcessEvent_ApiEvent_name */
+    /* Details: "Method name(): On an event request, the method is */
+    /* executed if the Event name matches the name field" */
+    {
+      PortletURL aurl = portletResp.createActionURL();
+      aurl.setParameters(portletReq.getPrivateParameterMap());
+      TestButton tb = new TestButton(V2ANNOTATIONTESTS_PROCESSEVENT_APIEVENT_NAME, aurl);
+      tb.writeTo(writer);
+    }
 
-      PrintWriter writer = portletResp.getWriter();
-
-      /* TestCase: V2AnnotationTests_ProcessEvent_ApiEvent_qname              */
-      /* Details: "Method qname(): On an event request, the method is         */
-      /* executed if the Event qname matches the qname field"                 */
-      {
-         PortletURL aurl = portletResp.createActionURL();
-         aurl.setParameters(portletReq.getPrivateParameterMap());
-         TestButton tb = new TestButton(V2ANNOTATIONTESTS_PROCESSEVENT_APIEVENT_QNAME, aurl);
-         tb.writeTo(writer);
-      }
-
-      /* TestCase: V2AnnotationTests_ProcessEvent_ApiEvent_name               */
-      /* Details: "Method name(): On an event request, the method is          */
-      /* executed if the Event name matches the name field"                   */
-      {
-         PortletURL aurl = portletResp.createActionURL();
-         aurl.setParameters(portletReq.getPrivateParameterMap());
-         TestButton tb = new TestButton(V2ANNOTATIONTESTS_PROCESSEVENT_APIEVENT_NAME, aurl);
-         tb.writeTo(writer);
-      }
-
-   }
+  }
 
 }
--- portlet-tck_3.0/V2AnnotationTests/src/main/java/javax/portlet/tck/portlets/AnnotationTests_RenderMode_ApiRender.java
+++ portlet-tck_3.0/V2AnnotationTests/src/main/java/javax/portlet/tck/portlets/AnnotationTests_RenderMode_ApiRender.java
@@ -1,19 +1,16 @@
-/*  Licensed to the Apache Software Foundation (ASF) under one
- *  or more contributor license agreements.  See the NOTICE file
- *  distributed with this work for additional information
- *  regarding copyright ownership.  The ASF licenses this file
- *  to you under the Apache License, Version 2.0 (the
- *  "License"); you may not use this file except in compliance
- *  with the License.  You may obtain a copy of the License at
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License. You may obtain a
+ * copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing,
- *  software distributed under the License is distributed on an
- *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- *  KIND, either express or implied.  See the License for the
- *  specific language governing permissions and limitations
- *  under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 
 package javax.portlet.tck.portlets;
@@ -23,7 +20,6 @@ import static javax.portlet.tck.constants.Constants.THREADID_ATTR;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.logging.Logger;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
@@ -32,87 +28,59 @@ import javax.portlet.PortletException;
 import javax.portlet.RenderMode;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
-import javax.portlet.ResourceRequest;
-import javax.portlet.ResourceResponse;
 import javax.portlet.tck.beans.JSR286ApiTestCaseDetails;
 import javax.portlet.tck.beans.TestResult;
 
 /**
- * This portlet implements several test cases for the JSR 362 TCK. The test case names
- * are defined in the /src/main/resources/xml-resources/additionalTCs.xml
- * file. The build process will integrate the test case names defined in the 
- * additionalTCs.xml file into the complete list of test case names for execution by the driver.
+ * This portlet implements several test cases for the JSR 362 TCK. The test case names are defined
+ * in the /src/main/resources/xml-resources/additionalTCs.xml file. The build process will integrate
+ * the test case names defined in the additionalTCs.xml file into the complete list of test case
+ * names for execution by the driver.
  *
- * This is the main portlet for the test cases. If the test cases call for events, this portlet
- * will initiate the events, but not process them. The processing is done in the companion 
- * portlet AnnotationTests_RenderMode_ApiRender_event
+ * This is the main portlet for the test cases. If the test cases call for events, this portlet will
+ * initiate the events, but not process them. The processing is done in the companion portlet
+ * AnnotationTests_RenderMode_ApiRender_event
  *
  */
 public class AnnotationTests_RenderMode_ApiRender extends GenericPortlet {
-   private static final String LOG_CLASS = 
-         AnnotationTests_RenderMode_ApiRender.class.getName();
-   private final Logger LOGGER = Logger.getLogger(LOG_CLASS);
-   
-   
 
-   @Override
-   public void init() {
-	  
-   }
-   
-   @Override
-   public void destroy() {
-   }
+  @Override
+  public void processAction(ActionRequest portletReq, ActionResponse portletResp)
+      throws PortletException, IOException {
 
-   @Override
-   public void processAction(ActionRequest portletReq, ActionResponse portletResp)
-         throws PortletException, IOException {
-      LOGGER.entering(LOG_CLASS, "main portlet processAction entry");
+    portletResp.setRenderParameters(portletReq.getParameterMap());
+    long tid = Thread.currentThread().getId();
+    portletReq.setAttribute(THREADID_ATTR, tid);
 
-      portletResp.setRenderParameters(portletReq.getParameterMap());
-      long tid = Thread.currentThread().getId();
-      portletReq.setAttribute(THREADID_ATTR, tid);
+  }
 
-   }
+  @RenderMode(name = "VIEW")
+  public void newRender(RenderRequest req, RenderResponse resp)
+      throws PortletException, IOException {
+    PrintWriter writer = resp.getWriter();
+    JSR286ApiTestCaseDetails tcd = new JSR286ApiTestCaseDetails();
 
-   @Override
-   public void serveResource(ResourceRequest portletReq, ResourceResponse portletResp)
-         throws PortletException, IOException {
-      LOGGER.entering(LOG_CLASS, "main portlet serveResource entry");
 
-      long tid = Thread.currentThread().getId();
-      portletReq.setAttribute(THREADID_ATTR, tid);
+    /* TestCase: V2AnnotationTests_RenderMode_ApiRender_name */
+    /* Details: "Method name(): On a render request, the method is */
+    /* executed if the portlet mode matches the name field" */
 
-   }
-   
-   @RenderMode(name="VIEW")
-   public void newRender(RenderRequest req,RenderResponse resp)
-           throws PortletException,IOException {
-	   PrintWriter writer = resp.getWriter();
-	   JSR286ApiTestCaseDetails tcd = new JSR286ApiTestCaseDetails();
-	   
-	   
-	  /* TestCase: V2AnnotationTests_RenderMode_ApiRender_name                */
-	  /* Details: "Method name(): On a render request, the method is          */
-	  /* executed if the portlet mode matches the name field"                 */
-	   
-	  TestResult tr0 = tcd.getTestResultFailed(V2ANNOTATIONTESTS_RENDERMODE_APIRENDER_NAME);
-	   tr0.setTcSuccess(true);
-	   tr0.writeTo(writer);
+    TestResult tr0 = tcd.getTestResultFailed(V2ANNOTATIONTESTS_RENDERMODE_APIRENDER_NAME);
+    tr0.setTcSuccess(true);
+    tr0.writeTo(writer);
 
-   }
+  }
 
-   @Override
-   public void render(RenderRequest portletReq, RenderResponse portletResp)
-         throws PortletException, IOException {
-      LOGGER.entering(LOG_CLASS, "main portlet render entry");
+  @Override
+  public void render(RenderRequest portletReq, RenderResponse portletResp)
+      throws PortletException, IOException {
 
-      long tid = Thread.currentThread().getId();
-      portletReq.setAttribute(THREADID_ATTR, tid);
-      
-      super.render(portletReq, portletResp);
-      
+    long tid = Thread.currentThread().getId();
+    portletReq.setAttribute(THREADID_ATTR, tid);
 
-   }
+    super.render(portletReq, portletResp);
+
+
+  }
 
 }
