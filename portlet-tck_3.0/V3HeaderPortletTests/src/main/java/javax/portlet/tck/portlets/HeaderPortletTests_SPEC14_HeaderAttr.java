006b6de1a7c7cabd71d954c2ee80ab2e5eeb408b
ahmed <mahmedk91@gmail.com>
2016-07-18 13:36:16 +0200

    Implemented all tests of HeaderPortletTests_SPEC14_HeaderAttr

--- portlet-tck_3.0/V3HeaderPortletTests/src/main/java/javax/portlet/tck/portlets/HeaderPortletTests_SPEC14_HeaderAttr.java
+++ portlet-tck_3.0/V3HeaderPortletTests/src/main/java/javax/portlet/tck/portlets/HeaderPortletTests_SPEC14_HeaderAttr.java
@@ -18,40 +18,46 @@
 
 package javax.portlet.tck.portlets;
 
-import java.io.*;
-import java.util.*;
-import java.util.logging.*;
-import static java.util.logging.Logger.*;
-import javax.xml.namespace.QName;
-import javax.portlet.*;
-import javax.portlet.annotations.*;
-import javax.portlet.filter.*;
-import javax.servlet.*;
-import javax.servlet.http.*;
-import javax.portlet.tck.beans.*;
-import javax.portlet.tck.constants.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+import javax.portlet.HeaderPortlet;
+import javax.portlet.HeaderRequest;
+import javax.portlet.HeaderResponse;
+import javax.portlet.Portlet;
+import javax.portlet.PortletConfig;
+import javax.portlet.PortletException;
+import javax.portlet.PortletRequest;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+import javax.portlet.annotations.PortletConfiguration;
+import javax.portlet.tck.beans.TestResult;
 import javax.portlet.tck.util.ModuleTestCaseDetails;
-import static javax.portlet.tck.util.ModuleTestCaseDetails.*;
-import static javax.portlet.tck.constants.Constants.*;
-import static javax.portlet.PortletSession.*;
-import static javax.portlet.ResourceURL.*;
+
+import static javax.portlet.tck.util.ModuleTestCaseDetails.V3HEADERPORTLETTESTS_SPEC14_HEADERATTR_ATTRIBUTES1;
+import static javax.portlet.tck.util.ModuleTestCaseDetails.V3HEADERPORTLETTESTS_SPEC14_HEADERATTR_ATTRIBUTES2;
+import static javax.portlet.tck.util.ModuleTestCaseDetails.V3HEADERPORTLETTESTS_SPEC14_HEADERATTR_ATTRIBUTES3;
+import static javax.portlet.tck.constants.Constants.RESULT_ATTR_PREFIX;
+import static javax.portlet.PortletSession.APPLICATION_SCOPE;
 
 /**
- * This portlet implements several test cases for the JSR 362 TCK. The test case names
- * are defined in the /src/main/resources/xml-resources/additionalTCs.xml
- * file. The build process will integrate the test case names defined in the 
- * additionalTCs.xml file into the complete list of test case names for execution by the driver.
+ * This portlet implements several test cases for the JSR 362 TCK. The test case
+ * names are defined in the /src/main/resources/xml-resources/additionalTCs.xml
+ * file. The build process will integrate the test case names defined in the
+ * additionalTCs.xml file into the complete list of test case names for
+ * execution by the driver.
  *
  */
 
 @PortletConfiguration(portletName = "HeaderPortletTests_SPEC14_HeaderAttr")
-public class HeaderPortletTests_SPEC14_HeaderAttr implements Portlet {
-   
-   private PortletConfig portletConfig = null;
+public class HeaderPortletTests_SPEC14_HeaderAttr
+      implements HeaderPortlet, Portlet {
 
    @Override
    public void init(PortletConfig config) throws PortletException {
-      this.portletConfig = config;
    }
 
    @Override
@@ -59,46 +65,81 @@ public class HeaderPortletTests_SPEC14_HeaderAttr implements Portlet {
    }
 
    @Override
-   public void processAction(ActionRequest portletReq, ActionResponse portletResp) throws PortletException, IOException {
+   public void processAction(ActionRequest portletReq,
+         ActionResponse portletResp) throws PortletException, IOException {
    }
 
    @Override
-   public void render(RenderRequest portletReq, RenderResponse portletResp) throws PortletException, IOException {
+   public void render(RenderRequest portletReq, RenderResponse portletResp)
+         throws PortletException, IOException {
 
       PrintWriter writer = portletResp.getWriter();
+      String msg = (String) portletReq.getPortletSession().getAttribute(
+            RESULT_ATTR_PREFIX + "HeaderPortletTests_SPEC14_HeaderAttr",
+            APPLICATION_SCOPE);
+      msg = (msg.equals("nullnull")) ? "Not ready. click test case link." : msg;
+      writer.write("<p>" + msg + "</p>\n");
+      portletReq.getPortletSession().removeAttribute(
+            RESULT_ATTR_PREFIX + "HeaderPortletTests_SPEC14_HeaderAttr",
+            APPLICATION_SCOPE);
+   }
+
+   @Override
+   public void renderHeaders(HeaderRequest portletReq,
+         HeaderResponse PortletResp) throws PortletException, IOException {
+
+      StringWriter writer = new StringWriter();
+
       ModuleTestCaseDetails tcd = new ModuleTestCaseDetails();
 
-      /* TestCase: V3HeaderPortletTests_SPEC14_HeaderAttr_attributes1               */
-      /* Details: "The portlet can access a map with user information attributes    */
-      /* via the request attribute PortletRequest.USER_INFO"                        */
+      /* TestCase: V3HeaderPortletTests_SPEC14_HeaderAttr_attributes1 */
+      /*
+       * Details: "The portlet can access a map with user information attributes
+       * via the request attribute PortletRequest.USER_INFO"
+       */
       {
-         TestResult result = tcd.getTestResultFailed(V3HEADERPORTLETTESTS_SPEC14_HEADERATTR_ATTRIBUTES1);
-         /* TODO: implement test */
-         result.appendTcDetail("Not implemented.");
+         TestResult result = tcd.getTestResultFailed(
+               V3HEADERPORTLETTESTS_SPEC14_HEADERATTR_ATTRIBUTES1);
+         if (portletReq.getAttribute(PortletRequest.USER_INFO) != null) {
+            result.setTcSuccess(true);
+         }
          result.writeTo(writer);
       }
 
-      /* TestCase: V3HeaderPortletTests_SPEC14_HeaderAttr_attributes2               */
-      /* Details: "The PortletRequest.CCPP_PROFILE request attribute must return a  */
-      /* javax.ccpp.Profile based on the current portlet request"                   */
+      /* TestCase: V3HeaderPortletTests_SPEC14_HeaderAttr_attributes2 */
+      /*
+       * Details: "The PortletRequest.CCPP_PROFILE request attribute must return
+       * a javax.ccpp.Profile based on the current portlet request"
+       */
       {
-         TestResult result = tcd.getTestResultFailed(V3HEADERPORTLETTESTS_SPEC14_HEADERATTR_ATTRIBUTES2);
-         /* TODO: implement test */
-         result.appendTcDetail("Not implemented.");
+         TestResult result = tcd.getTestResultFailed(
+               V3HEADERPORTLETTESTS_SPEC14_HEADERATTR_ATTRIBUTES2);
+         if (portletReq.getAttribute(PortletRequest.CCPP_PROFILE) != null) {
+            result.setTcSuccess(true);
+         }
          result.writeTo(writer);
       }
 
-      /* TestCase: V3HeaderPortletTests_SPEC14_HeaderAttr_attributes3               */
-      /* Details: "During header processing, the LIFECYCLE_PHASE                    */
-      /* (\"javax.portlet.lifecycle_phase\") attribute will contain the string      */
-      /* \"HEADER_PHASE\""                                                          */
+      /* TestCase: V3HeaderPortletTests_SPEC14_HeaderAttr_attributes3 */
+      /*
+       * Details: "During header processing, the LIFECYCLE_PHASE
+       * (\"javax.portlet.lifecycle_phase\") attribute will contain the string
+       * \"HEADER_PHASE\""
+       */
       {
-         TestResult result = tcd.getTestResultFailed(V3HEADERPORTLETTESTS_SPEC14_HEADERATTR_ATTRIBUTES3);
-         /* TODO: implement test */
-         result.appendTcDetail("Not implemented.");
+         TestResult result = tcd.getTestResultFailed(
+               V3HEADERPORTLETTESTS_SPEC14_HEADERATTR_ATTRIBUTES3);
+         if (portletReq.getAttribute(PortletRequest.LIFECYCLE_PHASE) != null
+               && portletReq.getAttribute(PortletRequest.LIFECYCLE_PHASE)
+                     .equals("HEADER_PHASE")) {
+            result.setTcSuccess(true);
+         }
          result.writeTo(writer);
       }
 
+      portletReq.getPortletSession().setAttribute(
+            RESULT_ATTR_PREFIX + "HeaderPortletTests_SPEC14_HeaderAttr",
+            writer.toString(), APPLICATION_SCOPE);
    }
 
 }
