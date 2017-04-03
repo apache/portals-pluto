4f89abbc1e2bc781126c61a63dec2c4c3694456a
Neil Griffin <neil.griffin.scm@gmail.com>
2017-04-03 17:13:25 -0400

    PLUTO-655 Maven archetypes for Portlet 3.0

---
+++ maven-archetypes/bean-portlet/src/main/resources/archetype-resources/src/main/java/__packageInPathFormat__/HelloWorldPortlet.java
@@ -0,0 +1,48 @@
+package ${package};
+
+import javax.inject.Inject;
+
+import javax.portlet.HeaderRequest;
+import javax.portlet.HeaderResponse;
+import javax.portlet.PortletConfig;
+
+import javax.portlet.annotations.HeaderMethod;
+import javax.portlet.annotations.InitParameter;
+import javax.portlet.annotations.LocaleString;
+import javax.portlet.annotations.PortletConfiguration;
+import javax.portlet.annotations.RenderMethod;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+@PortletConfiguration(
+	portletName = "hello_world",
+	title = @LocaleString(value = "${artifactId}"),
+	initParams = @InitParameter(name = "foo", value = "1234"))
+public class HelloWorldPortlet {
+
+	private static final Logger logger = LogManager.getLogger(HelloWorldPortlet.class);
+
+	private static final String JSPX = "/WEB-INF/views/portletViewMode.jspx";
+
+	@Inject HeaderRequest headerRequest;
+	@Inject HeaderResponse headerResponse;
+	@Inject PortletConfig portletConfig;
+
+	@HeaderMethod(portletNames = {"hello_world"})
+	public void renderHeaders() {
+
+		String contextPath = headerRequest.getContextPath();
+		String linkMarkup = "<link rel=\"stylesheet\" type=\"text/css\" href=\"" +
+			contextPath + "/resources/css/main.css\"></link>";
+		headerResponse.addDependency("main.css", "${package}", null, linkMarkup);
+		logger.debug("[HEADER_PHASE] Added resource: " + linkMarkup);
+	}
+
+	@RenderMethod(include = JSPX, portletNames = {"hello_world"})
+	public void renderView() {
+		String foo = portletConfig.getInitParameter("foo");
+		logger.debug("[RENDER_PHASE] init-param foo: " + foo);
+		logger.debug("[RENDER_PHASE] Rendering view: " + JSPX);
+	}
+}
---
+++ maven-archetypes/generic-portlet/src/main/resources/archetype-resources/src/main/java/__packageInPathFormat__/HelloWorldPortlet.java
@@ -0,0 +1,41 @@
+package ${package};
+
+import java.io.IOException;
+
+import javax.portlet.GenericPortlet;
+import javax.portlet.HeaderRequest;
+import javax.portlet.HeaderResponse;
+import javax.portlet.PortletException;
+import javax.portlet.PortletRequestDispatcher;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+public class HelloWorldPortlet extends GenericPortlet {
+
+	private static final Logger logger = LogManager.getLogger(HelloWorldPortlet.class);
+
+	public void renderHeaders(HeaderRequest headerRequest, HeaderResponse headerResponse)
+		throws PortletException, IOException {
+
+		String contextPath = headerRequest.getContextPath();
+		String linkMarkup = "<link rel=\"stylesheet\" type=\"text/css\" href=\"" +
+			contextPath + "/resources/css/main.css\"></link>";
+		headerResponse.addDependency("main.css", "${package}", null, linkMarkup);
+		logger.debug("[HEADER_PHASE] Added resource: " + linkMarkup);
+	}
+
+	protected void doView(RenderRequest renderRequest, RenderResponse renderResponse)
+		throws PortletException, IOException {
+
+		String foo = getPortletConfig().getInitParameter("foo");
+		logger.debug("[RENDER_PHASE] init-param foo: " + foo);
+		String viewPath = "/WEB-INF/views/portletViewMode.jspx";
+		PortletRequestDispatcher portletRequestDispatcher =
+			getPortletContext().getRequestDispatcher(viewPath);
+		portletRequestDispatcher.include(renderRequest, renderResponse);
+		logger.debug("[RENDER_PHASE] Rendering view: " + viewPath);
+	}
+}
